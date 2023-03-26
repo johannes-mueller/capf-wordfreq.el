f3cbce96a587354eac1f4ee06278d088c83d26a9
Johannes Mueller <github@johannes-mueller.org>
2023-03-26 11:24:15 +0200

    Make it asynchronous

--- capf-wordfreq.el
+++ capf-wordfreq.el
@@ -19,6 +19,9 @@
 
 ;;; Commentary:
 
+(defvar-local capf-wordfreq--begin nil)
+(defvar-local capf-wordfreq--cands nil)
+
 (defun capf-wordfreq--default-path ()
   "Set up the default for `capf-wordfreq-path'."
   (concat (file-name-as-directory user-emacs-directory) "wordfreq-dicts"))
@@ -41,7 +44,7 @@ the current buffer."
   "Determine the path of the word list file."
   (when-let* ((dct ispell-local-dictionary)
               (dct-file (concat (file-name-as-directory capf-wordfreq-path) dct ".txt")))
-    (if (file-exists-p dct-file) dct-file)))
+    (if (file-exists-p dct-file) (expand-file-name dct-file))))
 
 (defvar capf-wordfreq--cached-grep-executable nil)
 
@@ -81,14 +84,84 @@ the current buffer."
     (capf-wordfreq--drop-empty-last-candidate
      (split-string (capf-wordfreq--fetch-candidates-raw prefix) "\n")))))
 
+(defvar-local capf-wordfreq--msg-fragment "")
+
+(defun capf-wordfreq--process-msg (msg)
+  (let* ((last-message (car (last (split-string (string-trim-right msg) "[\n]"))))
+         (fields (split-string last-message))
+         (beg (string-to-number (car fields)))
+         (prefix (car (cdr fields)))
+         (cands (mapcar
+                 (lambda (cand) (capf-wordfreq--enforce-exact-prefix cand prefix))
+                 (cddr fields))))
+    (setq capf-wordfreq--msg-fragment "")
+    (setq capf-wordfreq--begin (when cands beg)
+          capf-wordfreq--cands cands)))
+
+(defun capf-wordfreq--return-buffer-filter (_proc msg)
+  (if (string-suffix-p "\n" msg)
+      (capf-wordfreq--process-msg (concat capf-wordfreq--msg-fragment msg))
+    (setq capf-wordfreq--msg-fragment (concat capf-wordfreq--msg-fragment msg))))
+
+
+(defun capf-wordfreq--external-process-observer (&rest _)
+  (if-let* ((dict-file (capf-wordfreq--dictionary))
+            (bounds (bounds-of-thing-at-point 'word))
+	    (beg (car bounds))
+	    (end (cdr bounds))
+	    (prefix (buffer-substring-no-properties beg end)))
+      (process-send-string
+       (capf-wordfreq--start-external-process)
+       (format "%s %s %s\n" beg dict-file prefix))
+    (setq capf-wordfreq--begin nil
+          capf-wordfreq--cands nil)))
+
+
+(defconst capf-wordfreq--script "
+while read -r line; do
+    split=($line)
+    beg=${split[0]}
+    dictfile=${split[1]}
+    prefix=${split[2]}
+    grep_command='grep -i ^\"$prefix\" $dictfile | tr \"\\n\" \" \"'
+    words=\$(eval $grep_command)
+    echo \"$beg $prefix $words\"
+done
+")
+
+(defvar capf-wordfreq--external-process nil)
+(defvar capf-wordfreq--begin nil)
+(defvar capf-wordfreq--cands nil)
+
+(defun capf-wordfreq--start-external-process ()
+  (unless (capf-wordfreq--external-process-live-p)
+    (ignore-errors (delete-process capf-wordfreq--external-process))
+    (setq capf-wordfreq--external-process
+          (make-process :name "capf-wordfreq-external"
+                        :command `("/bin/bash" "-c" ,capf-wordfreq--script)
+                        :noquery t
+                        :stderr "*external-stderr*"
+                        :filter #'capf-wordfreq--return-buffer-filter)))
+  capf-wordfreq--external-process)
+
+
+(defun capf-wordfreq--external-process-live-p ()
+  (process-live-p capf-wordfreq--external-process))
+
+
+(defun capf-wordfreq--enforce-min-length (cands)
+  (seq-filter
+   (lambda (cand) (>= (length cand) capf-wordfreq-minimal-candidate-length))
+   cands))
+
+
 ;;;###autoload
 (defun capf-wordfreq-completion-at-point-function ()
   "The completion at point function."
-  (when-let* ((bounds (bounds-of-thing-at-point 'word))
-	      (beg (car bounds))
-	      (end (cdr bounds))
-	      (prefix (buffer-substring-no-properties beg end)))
-    `(,beg ,end ,(capf-wordfreq--candidates prefix))))
+  (if (and capf-wordfreq--begin capf-wordfreq--cands)
+      (list capf-wordfreq--begin (point) (capf-wordfreq--enforce-min-length capf-wordfreq--cands))
+    (add-hook 'after-change-functions #'capf-wordfreq--external-process-observer nil 'local)))
+
 
 (provide 'capf-wordfreq)
 ;;; capf-wordfreq.el ends here
--- test/capf-wordfreq.el-test.el
+++ test/capf-wordfreq.el-test.el
@@ -25,101 +25,158 @@
   (let ((ispell-local-dictionary nil))
     (should (equal (capf-wordfreq--dictionary) nil))))
 
-(ert-deftest test-no-ispell-dictionary-candidates-empty-list()
-  (let ((ispell-local-dictionary nil))
-    (should (equal (capf-wordfreq--candidates "foo") '()))))
 
-(ert-deftest test-candidates-foo ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/path/to/grep-program -i \\^foo /path/to/dict.txt")
-                                          :output "foobar\nfoobaz\nfoo\n")))
-               (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
-    (should (equal (capf-wordfreq--candidates "foo") '("foobar" "foobaz" "foo")))))
+(ert-deftest test-candidates-filter ()
+  (dolist (struct '(("23 foo foo foobar\n" 23 ("foo" "foobar"))
+                    ("42 bar barfoo bar\n" 42 ("barfoo" "bar"))
+                    ("23 Foo foo foobar\n" 23 ("Foo" "Foobar"))))
+    (let ((contents (pop struct))
+          (expected-begin (pop struct))
+          (expected-cands (pop struct))
+          (capf-wordfreq--begin nil)
+          (capf-wordfreq--cands nil))
+      (insert contents)
+      (capf-wordfreq--return-buffer-filter nil contents)
+      (should (equal capf-wordfreq--begin expected-begin))
+      (should (equal capf-wordfreq--cands expected-cands)))))
 
-(ert-deftest test-candidates-foo-case-sensitive ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/path/to/grep-program -i \\^Foo /path/to/dict.txt")
-                                          :output "foobar\nfoobaz\nfoo\n")))
-               (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
-    (should (equal (capf-wordfreq--candidates "Foo") '("Foobar" "Foobaz" "Foo")))))
 
-(ert-deftest test-candidates-bar ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/other/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/other/path/to/grep-program -i \\^bar /other/path/to/dict.txt")
-                                          :output "barbar\nbarbaz\nbar\n")))
-               (capf-wordfreq--dictionary () ((:output "/other/path/to/dict.txt"))))
-    (should (equal (capf-wordfreq--candidates "bar") '("barbar" "barbaz" "bar")))))
+(ert-deftest test-candidates-filter-two-messages ()
+  (capf-wordfreq--return-buffer-filter nil "23 foo foo foobar\n42 bar barfoo bar\n")
+    (should (equal capf-wordfreq--begin 42))
+      (should (equal capf-wordfreq--cands '("barfoo" "bar"))))
 
-(ert-deftest test-candidates-no-newline-at-end ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/path/to/grep-program -i \\^foo /path/to/dict.txt")
-                                          :output "foobar\nfoobaz\nfoo")))
-               (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
-    (should (equal (capf-wordfreq--candidates "foo") '("foobar" "foobaz" "foo")))))
 
-(ert-deftest test-candidates-foo-minimal-candidate-length ()
-  (let ((capf-wordfreq-minimal-candidate-length 4))
-    (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	         (shell-command-to-string (command)
-                                          ((:input
-                                            '("/path/to/grep-program -i \\^foo /path/to/dict.txt")
-                                            :output "foobar\nfoob\nfoo\n")))
-                 (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
-      (should (equal (capf-wordfreq--candidates "foo") '("foobar" "foob"))))))
+(ert-deftest test-candidates-filter-fragmented-message ()
+  (let ((capf-wordfreq--begin 23)
+        (capf-wordfreq--cands '("foo" "foobar")))
+    (capf-wordfreq--return-buffer-filter nil "23 Foo foo")
+    (should (equal capf-wordfreq--begin 23))
+    (should (equal capf-wordfreq--cands '("foo" "foobar")))
+    (should (equal capf-wordfreq--msg-fragment "23 Foo foo"))))
 
-(ert-deftest test-dict-file-not-found ()
-  (mocker-let ((capf-wordfreq--dictionary () ((:output nil))))
-    (should (equal (capf-wordfreq--candidates "foo") '()))))
 
-(ert-deftest test-capf-completion-at-point-function ()
-  (let ((buffer-contents "f b"))
-    (mocker-let ((capf-wordfreq--candidates (prefix)
-					    ((:input '("f") :output '("foo"))
-					     (:input '("b") :output '("baz" "bar")))))
-      (setq completion-at-point-functions '(capf-wordfreq-completion-at-point-function))
-      (with-temp-buffer
-	(insert buffer-contents)
-	(goto-char (point-min))
-	(forward-char 1)
-	(completion-at-point)
-	(should (equal (buffer-string) "foo b"))
-	(forward-char 1)
-	(completion-at-point)
-	(should (equal (buffer-string) "foo ba"))))))
+(ert-deftest test-candidates-filter-fragmented-message-second-part ()
+  (let ((capf-wordfreq--msg-fragment "23 Foo foo"))
+    (capf-wordfreq--return-buffer-filter nil " foobar\n")
+    (should (equal capf-wordfreq--begin 23))
+    (should (equal capf-wordfreq--cands '("Foo" "Foobar")))
+    (should (equal capf-wordfreq--msg-fragment ""))))
 
-(ert-deftest test-capf-completion-after-space ()
-  (mocker-let ((capf-wordfreq--candidates (prefix) ((:input '() :occur 0))))
-    (with-temp-buffer
-      (insert "foo  ")
-      (capf-wordfreq-completion-at-point-function))))
 
-(ert-deftest test-candidates-foo-in-buffer ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/path/to/grep-program -i \\^foo /path/to/dict.txt")
-                                          :output "foofoo\nfoobaz\nfoo\n")))
-               (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
+(ert-deftest test-candidates-filter-fragmented-message-third-part ()
+  (let ((capf-wordfreq--msg-fragment "23 Foo foo"))
+    (capf-wordfreq--return-buffer-filter nil " foo")
+    (capf-wordfreq--return-buffer-filter nil "bar\n")
+    (should (equal capf-wordfreq--begin 23))
+    (should (equal capf-wordfreq--cands '("Foo" "Foobar")))
+    (should (equal capf-wordfreq--msg-fragment ""))))
+
+
+(ert-deftest test-candidates-filter-no-candidates ()
+  (capf-wordfreq--return-buffer-filter nil "23 foo")
+    (should (equal capf-wordfreq--begin nil))
+      (should (equal capf-wordfreq--cands nil)))
+
+
+(ert-deftest test-observer-foo ()
+  (mocker-let ((capf-wordfreq--start-external-process () ((:input '() :output 'start-external-process)))
+               (capf-wordfreq--dictionary () ((:input '() :output "/foo/bar/esperanto.txt")))
+               (process-send-string (process string) ((:input '(start-external-process "1 /foo/bar/esperanto.txt foo\n")))))
     (with-temp-buffer
       (insert "foo")
-      (should (equal (capf-wordfreq-completion-at-point-function) '(1 4 ("foofoo" "foobaz" "foo")))))))
+      (capf-wordfreq--external-process-observer 'foo))))
 
-(ert-deftest test-candidates-bar-in-buffer ()
-  (mocker-let ((capf-wordfreq--grep-executable () ((:input '() :output "/path/to/grep-program")))
-	       (shell-command-to-string (command)
-                                        ((:input
-                                          '("/path/to/grep-program -i \\^ba /path/to/dict.txt")
-                                          :output "barbar\nbarbaz\nbar\n")))
-               (capf-wordfreq--dictionary () ((:output "/path/to/dict.txt"))))
+(ert-deftest test-observer-bar ()
+  (mocker-let ((capf-wordfreq--start-external-process () ((:input '() :output 'start-external-process)))
+               (capf-wordfreq--dictionary () ((:input '() :output "/foo/bar/deutsch.txt")))
+               (process-send-string (process string) ((:input '(start-external-process "5 /foo/bar/deutsch.txt bar\n")))))
     (with-temp-buffer
-      (insert "foo ba")
-      (when-let ((foo nil)))
-      (should (equal (capf-wordfreq-completion-at-point-function) '(5 7 ("barbar" "barbaz" "bar")))))))
+      (insert "foo bar")
+      (capf-wordfreq--external-process-observer 'foo))))
+
+
+(ert-deftest test-observer-dict-file-not-found ()
+  (mocker-let ((capf-wordfreq--dictionary () ((:output nil)))
+               (process-send-string (process string) ((:input '() :occur 0))))
+    (with-temp-buffer
+      (insert "foo bar")
+      (capf-wordfreq--external-process-observer 'foo))))
+
+
+(ert-deftest test-observer-nil ()
+  (mocker-let ((capf-wordfreq--dictionary () ((:output "/foo/bar/english.txt")))
+               (process-send-string (process string) ((:input '() :occur 0))))
+    (let ((capf-wordfreq--begin 17)
+          (capf-wordfreq--cands '("foo" "foobar")))
+      (with-temp-buffer
+        (insert "foo bar ")
+        (capf-wordfreq--external-process-observer 'foo)
+        (should (eq capf-wordfreq--begin nil))
+        (should (eq capf-wordfreq--cands nil))))))
+
+
+(ert-deftest test-start-external-process-still-live ()
+  (mocker-let ((capf-wordfreq--external-process-live-p () ((:input '() :output t))))
+    (let ((capf-wordfreq--external-process 'foo))
+      (should (eq (capf-wordfreq--start-external-process) 'foo)))
+    (let ((capf-wordfreq--external-process 'bar))
+      (should (eq (capf-wordfreq--start-external-process) 'bar)))))
+
+
+(ert-deftest test-start-external-process-not-live ()
+  (setq make-process-prms `(:name "capf-wordfreq-external"
+                            :command ,`("/bin/bash" "-c" ,capf-wordfreq--script)
+                            :noquery t
+                            :stderr "*external-stderr*"
+                            :filter capf-wordfreq--return-buffer-filter))
+  (mocker-let ((capf-wordfreq--external-process-live-p () ((:input '() :output nil)))
+               (delete-process (process) ((:input '(bfoo) :output nil :occur 1)
+                                          (:input '(bbar) :output nil :occur 1)))
+               (make-process (&rest _) ((:input make-process-prms :output 'foo :occur 1)
+                                        (:input make-process-prms :output 'bar :occur 1))))
+    (let ((capf-wordfreq--external-process 'bfoo))
+      (should (eq (capf-wordfreq--start-external-process) 'foo))
+      (should (eq capf-wordfreq--external-process 'foo)))
+    (let ((capf-wordfreq--external-process 'bbar))
+      (should (eq (capf-wordfreq--start-external-process) 'bar))
+      (should (eq capf-wordfreq--external-process 'bar)))))
+
+
+(ert-deftest test-external-process-live-p ()
+  (mocker-let ((process-live-p (process) ((:input '(foo) :output t)
+                                          (:input '(bar) :output nil))))
+    (let ((capf-wordfreq--external-process 'foo))
+      (should (capf-wordfreq--external-process-live-p)))
+    (let ((capf-wordfreq--external-process 'bar))
+      (should (not (capf-wordfreq--external-process-live-p))))))
+
+
+(ert-deftest test-capf-wordfreq-completion-at-point-function-nil ()
+  (mocker-let ((add-hook (hook function depth local) ((:input
+                                                       '(after-change-functions
+                                                         capf-wordfreq--external-process-observer
+                                                         nil
+                                                         local)))))
+    (let ((capf-wordfreq--begin nil)
+          (capf-wordfreq--cands nil))
+      (should (eq (capf-wordfreq-completion-at-point-function) nil)))))
+
+
+(ert-deftest test-capf-wordfreq-completion-at-point-function-nonnil ()
+  (let ((capf-wordfreq--begin 23)
+        (capf-wordfreq--cands '("foobar" "foo")))
+    (mocker-let ((point () ((:input '() :output 25))))
+      (should (equal (capf-wordfreq-completion-at-point-function) '(23 25 ("foobar" "foo"))))))
+  (let ((capf-wordfreq--begin 42)
+        (capf-wordfreq--cands '("barfoo" "bar")))
+    (mocker-let ((point () ((:input '() :output 47))))
+      (should (equal (capf-wordfreq-completion-at-point-function) '(42 47 ("barfoo" "bar")))))))
+
+
+(ert-deftest test-capf-wordfreq-completion-at-point-function-minimal-candidate-length ()
+  (let ((capf-wordfreq--begin 23)
+        (capf-wordfreq--cands '("foobar" "foo"))
+        (capf-wordfreq-minimal-candidate-length 4))
+    (mocker-let ((point () ((:input '() :output 25))))
+      (should (equal (capf-wordfreq-completion-at-point-function) '(23 25 ("foobar")))))))
